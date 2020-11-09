af075bcfeccb3bfde85907dc0c044615f35d184e
Patrick Rodrigues <47316946+RodPaDev@users.noreply.github.com>
2020-11-09 17:21:21 +0000

    update server

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,13 @@
-import express, {Application, Request, Response} from "express";
+import express, {Application, json, Request, Response} from "express";
 import helmet from 'helmet';
 
 export const app: Application = express();
-
 app.use(helmet())
+app.use(json())
+
+import userRoute from '../api/routes/usersRoute';
+
+app.use('/users', userRoute)
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Soon you'll be watering some plants!")
