71f6da4a4df13bc880ae06cd0f4be303dcfab424
Patrick Rodrigues <47316946+RodPaDev@users.noreply.github.com>
2020-11-09 19:56:16 +0000

    add custom type

--- custom_typings/index.d.ts
+++ custom_typings/index.d.ts
@@ -3,6 +3,13 @@ declare namespace Express {
         user: {
             email: string,
             password: string
+        },
+        userInfo: {
+            user_id: number,
+            username: string,
+            first_name:  String,
+            last_name:  String,
+            profile_pic: String,
         }
     }
 }
